f0a3594660621a060042bea1c4b39150fecd20c5
Felix C. Stegerman <flx@obfusk.net>
2013-07-17 19:25:17 +0200

    Arg{,ument}Error

--- lib/obfusk/util/valid.rb
+++ lib/obfusk/util/valid.rb
@@ -17,7 +17,7 @@ module Obfusk; module Util; module Valid
   # --
 
   # validate #args in min..max (min.. if max=nil); returns args
-  # @raise ArgError on out of bounds
+  # @raise ArgumentError on out of bounds
   def self.args(what, args, min, max = min)
     if (l = args.length) < min || (max && l > max)
       raise ArgumentError,
