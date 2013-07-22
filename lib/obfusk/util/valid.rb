88782c12a8f56af193e863d0610d3e1626715484
Felix C. Stegerman <flx@obfusk.net>
2013-07-22 23:19:36 +0200

    optparse w/ officious

--- lib/obfusk/util/valid.rb
+++ lib/obfusk/util/valid.rb
@@ -2,7 +2,7 @@
 #
 # File        : obfusk/util/valid.rb
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2013-07-17
+# Date        : 2013-07-22
 #
 # Copyright   : Copyright (C) 2013  Felix C. Stegerman
 # Licence     : GPLv2
@@ -30,15 +30,31 @@ module Obfusk; module Util; module Valid
     raise ValidationError, msg
   end
 
-  # parse options, return remaining args; assumes optparse's .parse!
-  def self.parse_opts(op, args)
-    as = args.dup; op.parse! as; as
-  end
-
   # validate value against regex
   # @raise ValidationError on no match
   def self.validate!(x, rx, name)
     x.to_s.match /^(#{rx})$/ or invalid! "invalid #{name}"
+  end
+
+  # --
+
+  # disable optparse's officious options
+  def self.disable_optparse_officious(o, what = nil)
+    (what || OptionParser::Officious.keys).each do |x|
+      o.on("--#{x}") { raise OptionParser::InvalidOption }
+    end
+  end
+
+  # clean OptionParser
+  def self.optparse_clean(*args, &b)
+    OptionParser.new(*args) do |o|
+      disable_optparse_officious o; b[o] if b
+    end
+  end
+
+  # parse options, return remaining args; assumes optparse's .parse!
+  def self.parse_opts(op, args)
+    as = args.dup; op.parse! as; as
   end
 
 end; end; end
