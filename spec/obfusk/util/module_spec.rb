739531ac47d57408f26698361d3571b1de551dc5
Felix C. Stegerman <flx@obfusk.net>
2013-07-24 21:06:30 +0200

    module spec

---
+++ spec/obfusk/util/module_spec.rb
@@ -0,0 +1,56 @@
+# --                                                            ; {{{1
+#
+# File        : obfusk/util/module_spec.rb
+# Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+# Date        : 2013-07-24
+#
+# Copyright   : Copyright (C) 2013  Felix C. Stegerman
+# Licence     : GPLv2
+#
+# --                                                            ; }}}1
+
+require 'obfusk/util/module'
+
+require 'fileutils'
+require 'tmpdir'
+
+module Obfusk::Util::Module__Spec
+  module Foo; end; module Bar; end
+end
+
+ou = Obfusk::Util
+ms = ou::Module__Spec
+
+describe 'obfusk/util/module' do
+
+  context 'require_all' do                                      # {{{1
+    it 'require_all' do
+      Dir.mktmpdir do |dir|
+        d = "#{dir}/obfusk/util/spec__"
+        FileUtils.mkdir_p d
+        %w{ foo bar }.each do |x|
+          File.write "#{d}/#{x}.rb",
+            "module #{ms}::#{x.capitalize}; VALUE = '#{x}'; end"
+        end
+        $:.unshift dir
+        begin
+          ou.require_all 'obfusk/util/spec__'
+          ms::Foo::VALUE.should == 'foo'
+          ms::Bar::VALUE.should == 'bar'
+        ensure
+          $:.shift
+        end
+      end
+    end
+  end                                                           # }}}1
+
+  context 'submodules' do                                       # {{{1
+    it 'submodules' do
+      ou.submodules(ms).should == \
+        { 'foo' => ms::Foo, 'bar' => ms::Bar }
+    end
+  end                                                           # }}}1
+
+end
+
+# vim: set tw=70 sw=2 sts=2 et fdm=marker :
