6a0d20fee8134216b6a709a3d08f8e7b9940ec8b
Felix C. Stegerman <flx@obfusk.net>
2013-07-24 21:30:33 +0200

    os spec

---
+++ spec/obfusk/util/os_spec.rb
@@ -0,0 +1,41 @@
+# --                                                            ; {{{1
+#
+# File        : obfusk/util/os_spec.rb
+# Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+# Date        : 2013-07-24
+#
+# Copyright   : Copyright (C) 2013  Felix C. Stegerman
+# Licence     : GPLv2
+#
+# --                                                            ; }}}1
+
+require 'obfusk/util/os'
+
+os = Obfusk::Util::OS
+
+describe 'obfusk/util/os' do
+
+  context 'home' do                                             # {{{1
+    it 'my home' do
+      os.home.should == ENV['HOME']                             # ????
+    end
+    it 'root' do
+      os.home('root').should == '/root'                         # ????
+    end
+  end                                                           # }}}1
+
+  context 'user' do                                             # {{{1
+    it 'user' do
+      os.user.should == ENV['USER']                             # ????
+    end
+  end                                                           # }}}1
+
+  context 'now' do                                              # {{{1
+    it 'now' do
+      os.now.should match /^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$/
+    end
+  end                                                           # }}}1
+
+end
+
+# vim: set tw=70 sw=2 sts=2 et fdm=marker :
